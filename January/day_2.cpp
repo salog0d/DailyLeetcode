ac0a8434a8a276e49d6fbe964bcd72828a2260a0
salogod <david.salomon.nava11@gmail.com>
2025-01-02 16:34:08 -0600

    Day 2: Reverse Integer

---
+++ January/day_2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+/*
+Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value 
+to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*/
+
+//Solution 1: Time complexity o(log(x))
+int reverse(int x) {
+    int rev = 0;
+    while (x != 0) {
+        int pop = x % 10;
+        x /= 10;
+        if (rev > INT_MAX /10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+        if (rev < INT_MIN /10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
+
+// Solution 2: Using long to handle overflow Time complexity o(log(x))
+int reverse_alternate(int x) {
+    long rev = 0;
+    while (x != 0) {
+        int pop = x % 10;
+        x /= 10;
+        rev = rev * 10 + pop;
+        if (rev > INT_MAX || rev < INT_MIN) return 0;
+    }
+    return static_cast<int>(rev);
+}
+
+int main() {
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    cout << "Reversed integer: " << reverse(x) << endl;
+    return 0;
+}
